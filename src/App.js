2dad007a301b6a9e1c51723938683c2b9ed62131
Alex McGlothlin <Alex.McGlothlin@outlook.com>
2019-04-21 13:40:15 -0500

    work on components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
+
+const app = new Clarifai.App({
+  apiKey: '4665c453dc5743c186e0205861e8aa74'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +26,28 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => { 
+    console.log('click');  
+    app.models.predict(Clarifai.COLOR_MODEL,
+    // URL
+    "https://samples.clarifai.com/metro-north.jpg")
+    .then(function(response) {
+    console.log(response);},
+    function(err) {console.log(err);
+    });
+}
+
   render() {
     return (
       <div className="App">
@@ -29,8 +57,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm /> 
-                {/*<FaceRecognition />*/}
+        <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/> 
+        <FaceRecognition />
       </div>
     );
   }
